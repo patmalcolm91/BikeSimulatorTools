0710df86fb8136ccca2acd83d45f19853f95cec6
patmalcolm91 <patmalcolm91@gmail.com>
2021-04-26 17:44:33 +0200

    add support for "wait targets" in MultiConflict
    Conflict vehicle will slow down and wait at its wait target until the ego passes its target.
    Before reaching the wait target, the conflict vehicle will simply be controlled by SUMO.

--- MultiConflict.py
+++ MultiConflict.py
@@ -14,6 +14,10 @@ class _ConflictTarget(NamedTuple):
     release_point: float
 
 
+class _WaitTarget(_ConflictTarget):
+    pass
+
+
 class MultiConflict:
     """A class for coordinating two vehicles to arrive simultaneously at set points along their respective routes."""
     def __init__(self, ego_id, conflict_vehicle_id, ego_route=None, conflict_vehicle_route=None, release_point=10):
@@ -27,6 +31,7 @@ class MultiConflict:
         self._targets = []  # type: list[_ConflictTarget] # ego station, cv station, release point
         self.release_point = release_point
         self._active = False
+        self._waiting = False
 
     @staticmethod
     def _line_from_route(route_id):
@@ -37,22 +42,42 @@ class MultiConflict:
             coords += traci.lane.getShape(edge+"_0")
         return LineString(coords)
 
-    def add_target(self, ego_coords, conflict_vehicle_coords, release_point=None):
-        """Add a target to be managed."""
+    def _add_generic_target(self, target_class, ego_coords, conflict_vehicle_coords, release_point=None):
         release_point = release_point if release_point is not None else self.release_point
         _ep, _cvp = Point(ego_coords), Point(conflict_vehicle_coords)
         _e_station = self._ego_trajectory.project(_ep)
         _cv_station = self._conflict_vehicle_trajectory.project(_cvp)
-        self._targets.append(_ConflictTarget(_e_station, _cv_station, release_point))
+        self._targets.append(target_class(_e_station, _cv_station, release_point))
         self._active = True
 
-    def check(self):
-        """Check vehicle trajectories and adjust conflict vehicle speed as necessary."""
-        if len(self._targets) == 0:
-            if self._active:
-                self._active = False
-                traci.vehicle.setSpeed(self.conflict_vehicle_id, -1)
-            return None
+    def add_target(self, ego_coords, conflict_vehicle_coords, release_point=None):
+        """Add a target to be managed."""
+        self._add_generic_target(_ConflictTarget, ego_coords, conflict_vehicle_coords, release_point)
+
+    def add_wait_target(self, ego_coords, conflict_vehicle_coords, braking_distance=None):
+        """Add a wait target to be managed."""
+        self._add_generic_target(_WaitTarget, ego_coords, conflict_vehicle_coords, release_point=braking_distance)
+
+    def _control_wait_target(self):
+        ego_target, cv_target, release_point = self._targets[0]
+        ego_pos = traci.vehicle.getPosition(self.ego_id)
+        ego_station = self._ego_trajectory.project(Point(ego_pos))
+        cv_pos = traci.vehicle.getPosition(self.conflict_vehicle_id)
+        cv_station = self._conflict_vehicle_trajectory.project(Point(cv_pos))
+        if ego_station > ego_target:
+            self._targets.pop(0)
+            traci.vehicle.setSpeed(self.conflict_vehicle_id, -1)
+            self._waiting = False
+        elif cv_station < cv_target - release_point:
+            traci.vehicle.setSpeed(self.conflict_vehicle_id, -1)
+            self._waiting = False
+        else:
+            cv_speed = traci.vehicle.getSpeed(self.conflict_vehicle_id)
+            dur = (cv_target - cv_station) / cv_speed / 2
+            traci.vehicle.slowDown(self.conflict_vehicle_id, 0, dur)
+            self._waiting = True
+
+    def _control_conflict_target(self):
         ego_target, cv_target, release_point = self._targets[0]
         ego_speed = traci.vehicle.getSpeed(self.ego_id)
         if ego_speed == 0:
@@ -68,3 +93,17 @@ class MultiConflict:
             traci.vehicle.setSpeed(self.conflict_vehicle_id, cv_desired_speed)
         else:
             self._targets.pop(0)
+
+    def check(self):
+        """Check vehicle trajectories and adjust conflict vehicle speed as necessary."""
+        if len(self._targets) == 0:
+            if self._active:
+                self._active = False
+                traci.vehicle.setSpeed(self.conflict_vehicle_id, -1)
+            return None
+        if type(self._targets[0]) == _ConflictTarget:
+            self._control_conflict_target()
+        elif type(self._targets[0]) == _WaitTarget:
+            self._control_wait_target()
+        else:
+            raise NotImplementedError("Unsupported MultiConflict target type.")
